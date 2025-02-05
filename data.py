7212b380da9ab82dc682d5ab7cdd356740b95f41
aizudeen <aizudeenjapri@gmail.com>
2025-02-05 16:30:21 +0800

    update 5/2 new

--- data.py
+++ data.py
@@ -1,114 +1,183 @@
-import pandas as pd 
+import pandas as pd
 from pandasql import sqldf
 import datetime
+
+# use min max()
 
 class Data:
     def __init__(self):
         pass
+
     def vix_history(self):
-        df = pd.read_excel(r'C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Desktop\NLP Project\download zip\VIX_History.xlsx',sheet_name=1)
-        df['Date'] = df['Month'].astype(str) + '/' + df['Year'].astype(str)
-        df = df.rename(columns={'avg(CLOSE)': 'CLOSE'})
-        df = df[['Date', 'CLOSE']].iloc[::-1][360:]
+        df = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Desktop\NLP Project\download zip\VIX_History.xlsx",
+            sheet_name=1,
+        )
+        df["Date"] = df["Month"].astype(str) + "-" + df["Year"].astype(str)
+        df["Date"] = pd.to_datetime(df["Date"])#.dt.to_period('M') 
+        df = df.rename(columns={"avg(CLOSE)": "CLOSE"})
+        df = df[["Date", "CLOSE"]]#.iloc#[::-1]#[360:]
         return df
+
     def policy_rate(self):
-        df = pd.read_excel(r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Roger and Aizudeen\SEACEN CFM Data.xlsx",sheet_name=5, nrows=14)
-        df = df.drop(df.columns[[0,2,3]], axis = 1)
+        df = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Roger and Aizudeen\SEACEN CFM Data.xlsx",
+            sheet_name=5,
+            nrows=14,
+        )
+        df = df.drop(df.columns[[0, 2, 3]], axis=1)
         date = df.columns[1:]
         date2 = [x.date().strftime("%Y-%m") for x in date]
-        df = df.rename(columns={k:v for (k,v) in zip(date,date2)})
+        df = df.rename(columns={k: v for (k, v) in zip(date, date2)})
         df = df.drop(date2[:84], axis=1)
         df = df.melt(id_vars="Region", var_name="Date", value_name="Value")
-        df2 = sqldf('select * from df where Region in ("United States", "Euro Area", "Mongolia", "Nepal", "Philippines", "Korea", "Sri Lanka", "Thailand", "China")')
-        df3 = sqldf('select * from df where Region in ("Indonesia", "India", "Malaysia", "Chinese Taipei", "Vietnam")')
+        df2 = sqldf(
+            'select * from df where Region in ("United States", "Euro Area", "Mongolia", "Nepal", "Philippines", "Korea", "Sri Lanka", "Thailand", "China")'
+        )
+        df3 = sqldf(
+            'select * from df where Region in ("Indonesia", "India", "Malaysia", "Chinese Taipei", "Vietnam")'
+        )
         return df, df2, df3
+
     def monthly_inflation(self):
-        df = pd.read_excel(r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Growth and Inflation 3Q2024.xlsx", sheet_name=3, nrows=17)
-        df = df.drop(df.columns[[0,2,3]], axis = 1)
+        df = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Growth and Inflation 3Q2024.xlsx",
+            sheet_name=3,
+            nrows=17,
+        )
+        df = df.drop(df.columns[[0, 2, 3]], axis=1)
         date = df.columns[1:]
-        date_new =[x for x in date if isinstance(x, datetime.date)]
-        df = df[['Region', *date_new]]
+        date_new = [x for x in date if isinstance(x, datetime.date)]
+        df = df[["Region", *date_new]]
         date2 = [x.date().strftime("%Y-%m") for x in date]
-        df = df.rename(columns={k:v for (k,v) in zip(date,date2)})
+        df = df.rename(columns={k: v for (k, v) in zip(date, date2)})
         df = df.melt(id_vars="Region", var_name="Date", value_name="Value")
-        df2 = sqldf('select * from df where Region not in ("Cambodia", "China", "Hong Kong SAR (China)", "Malaysia", "Vietnam", "Thailand")')
-        df3 = sqldf('select * from df where Region in ("Cambodia", "China", "Hong Kong SAR (China)", "Malaysia", "Vietnam", "Thailand")')
+        df2 = sqldf(
+            'select * from df where Region not in ("Cambodia", "China", "Hong Kong SAR (China)", "Malaysia", "Vietnam", "Thailand")'
+        )
+        df3 = sqldf(
+            'select * from df where Region in ("Cambodia", "China", "Hong Kong SAR (China)", "Malaysia", "Vietnam", "Thailand")'
+        )
         return df, df2, df3
+
     def forex_exchange(self):
-        df1 = pd.read_excel(r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Section 1 Charts.xlsx", sheet_name=3, header=1)
-        df1 = df1.iloc[:,:12]
+        df1 = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Section 1 Charts.xlsx",
+            sheet_name=3,
+            header=1,
+        )
+        df1 = df1.iloc[:, :12]
         df2 = df1.copy()
         row = [x for x in range(362)]
-        df2 = df2.drop([0,1,*row], axis=0).reset_index().drop('index', axis=1)
-        df2 = df2.rename(columns={'Region':'Date'})
-        date_new =[x for x in df2.Date if isinstance(x, datetime.datetime)]
-        df2 = df2.iloc[:len(date_new)]
+        df2 = df2.drop([0, 1, *row], axis=0).reset_index().drop("index", axis=1)
+        df2 = df2.rename(columns={"Region": "Date"})
+        date_new = [x for x in df2.Date if isinstance(x, datetime.datetime)]
+        df2 = df2.iloc[: len(date_new)]
         df2 = df2.melt(id_vars="Date", var_name="Region", value_name="Value")
-        df2['Year'] = df2['Date'].apply(lambda x : x.date().strftime("%Y"))
-        df2['Date2'] = df2['Date'].apply(lambda x : x.date().strftime("%Y-%m"))
-        latest_year = int(sorted(df2['Year'].unique())[-1])
-        country = df2['Region'].unique()
+        df2["Year"] = df2["Date"].apply(lambda x: x.date().strftime("%Y"))
+        df2["Date2"] = df2["Date"].apply(lambda x: x.date().strftime("%Y-%m"))
+        latest_year = int(sorted(df2["Year"].unique())[-1])
+        country = df2["Region"].unique()
         country_year, all_same, year_use = dict(), True, latest_year
 
-        for x in country:    
-            if len(df2[(df2['Region']==x) & (df2['Year']==str(latest_year))]) > 0:
-                country_year.update({x:latest_year})
+        for x in country:
+            if len(df2[(df2["Region"] == x) & (df2["Year"] == str(latest_year))]) > 0:
+                country_year.update({x: latest_year})
             else:
                 all_same = False
-                year_use = df2[(df2['Region']==x)]['Year'].unique()[-1]
-                country_year.update({x:year_use}) 
+                year_use = df2[(df2["Region"] == x)]["Year"].unique()[-1]
+                country_year.update({x: year_use})
 
-        country_year.update({'info':{'all_same': all_same, 'year_use': year_use}})      
-        year1 = country_year['info']['year_use']
-        df2 = sqldf(f'select Date2 as Date, Year, Region, Value, row_number() over(partition by Region, Year order by Date2) as row_num from df2 where Year in ("{year1}", "{year1 - 1}")')
-        num_list = sorted(df2[(df2['Region']=='China') & (df2['Year']==str(year1))]['row_num'].unique())
-        num1, num2 =num_list[0] , num_list[-1]
-        df2 = df2[(df2['Region'].isin(country))&(df2['row_num'].isin([num1, num2]))&(df2['Year'].isin([str(year1), str(year1-1)]))]
-        calc_country, year_list = dict(), [str(year1), str(year1 -1)]
+        country_year.update({"info": {"all_same": all_same, "year_use": year_use}})
+        year1 = country_year["info"]["year_use"]
+        df2 = sqldf(
+            f'select Date2 as Date, Year, Region, Value, row_number() over(partition by Region, Year order by Date2) as row_num from df2 where Year in ("{year1}", "{year1 - 1}")'
+        )
+        num_list = sorted(
+            df2[(df2["Region"] == "China") & (df2["Year"] == str(year1))][
+                "row_num"
+            ].unique()
+        )
+        num1, num2 = num_list[0], num_list[-1]
+        df2 = df2[
+            (df2["Region"].isin(country))
+            & (df2["row_num"].isin([num1, num2]))
+            & (df2["Year"].isin([str(year1), str(year1 - 1)]))
+        ]
+        calc_country, year_list = dict(), [str(year1), str(year1 - 1)]
 
         for x in country:
             for y in year_list:
-                nomi = float(df2[(df2['Region'] == x)&(df2['row_num']==1)&(df2['Year']==str(y))]['Value'].values[0])
-                denomi = float(df2[(df2['Region'] == x)&(df2['row_num']!=1)&(df2['Year']==str(y))]['Value'].values[0])
-                calc = ((nomi/denomi)-1)*100
+                nomi = float(
+                    df2[
+                        (df2["Region"] == x)
+                        & (df2["row_num"] == 1)
+                        & (df2["Year"] == str(y))
+                    ]["Value"].values[0]
+                )
+                denomi = float(
+                    df2[
+                        (df2["Region"] == x)
+                        & (df2["row_num"] != 1)
+                        & (df2["Year"] == str(y))
+                    ]["Value"].values[0]
+                )
+                calc = ((nomi / denomi) - 1) * 100
                 if calc_country.get(x) == None:
-                    calc_country.update({x:{y:calc}})
+                    calc_country.update({x: {y: calc}})
                 else:
-                    calc_country[x].update({y:calc})
+                    calc_country[x].update({y: calc})
         df = pd.DataFrame(calc_country).reset_index()
-        df = df.melt(id_vars="index", var_name="Region", value_name="Value").rename({'index': 'Year'})
+        df = df.melt(id_vars="index", var_name="Region", value_name="Value").rename(
+            {"index": "Year"}
+        )
         return df
+
     def cds(self):
-        df = pd.read_excel(r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Section 1 Charts.xlsx", sheet_name=5, header=1)
-        col =df.columns[:10]
-        df2= df[[*col]]
-        i= [s.find(':') for s in col]
+        df = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Capital Flow Monitor\SEACEN CFM January 2025\edited files\Data\Section 1 Charts.xlsx",
+            sheet_name=5,
+            header=1,
+        )
+        col = df.columns[:10]
+        df2 = df[[*col]]
+        i = [s.find(":") for s in col]
         col2 = [x[:y] for x, y in zip(col[1:], i[1:])]
-        df2 = df2.rename({k:v for (k,v) in zip(col[1:],col2)}, axis=1).rename({'.DESC' : 'Date'}, axis=1)
-        to_include =[x for x in df2.China if isinstance(x, float)]
+        df2 = df2.rename({k: v for (k, v) in zip(col[1:], col2)}, axis=1).rename(
+            {".DESC": "Date"}, axis=1
+        )
+        to_include = [x for x in df2.China if isinstance(x, float)]
         df2 = df2[df2.China.isin(to_include)].reset_index(drop=True)
-        drop_index =[x for x in df2['Date'].isna()].index(True)
+        drop_index = [x for x in df2["Date"].isna()].index(True)
         df2 = df2.drop(index=[x for x in range(drop_index, len(df2))])
         df2 = df2.melt(id_vars="Date", var_name="Region", value_name="Value")
-        df2 = df2.fillna('')        
+        df2 = df2.fillna("")
         return df2
+
     def liquidity(self):
-        df = pd.read_excel(r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Desktop\NLP Project\download zip\BIS Liquidity data.xlsx")
-        df = df[['TIME_PERIOD','OBS_VALUE']]
+        df = pd.read_excel(
+            r"C:\Users\AhmadAizudeen\OneDrive - The SOUTH-EAST ASIAN CENTRAL BANKS (SEACEN) RESEARCH AND TRAINING\Desktop\NLP Project\download zip\BIS Liquidity data.xlsx"
+        )
+        df = df[["TIME_PERIOD", "OBS_VALUE"]]
         return df
+
     def all_df(self):
         vix = self.vix_history()
-        policy_rate1,policy_rate2,policy_rate3 = self.policy_rate()
-        monthly_inflation1,monthly_inflation2,monthly_inflation3 = self.monthly_inflation()
+        policy_rate1, policy_rate2, policy_rate3 = self.policy_rate()
+        monthly_inflation1, monthly_inflation2, monthly_inflation3 = (
+            self.monthly_inflation()
+        )
         forex_exchange = self.forex_exchange()
         cds = self.cds()
-        df_dict = {'vix':vix,
-                   'policy_rate1': policy_rate1,
-                   'policy_rate2': policy_rate2,
-                   'policy_rate3': policy_rate3,
-                   'monthly_inflation1': monthly_inflation1,
-                   'monthly_inflation2': monthly_inflation2,
-                   'monthly_inflation3': monthly_inflation3,
-                   'forex_exchange': forex_exchange,
-                   'cds':cds}
+        df_dict = {
+            "vix": vix,
+            "policy_rate1": policy_rate1,
+            "policy_rate2": policy_rate2,
+            "policy_rate3": policy_rate3,
+            "monthly_inflation1": monthly_inflation1,
+            "monthly_inflation2": monthly_inflation2,
+            "monthly_inflation3": monthly_inflation3,
+            "forex_exchange": forex_exchange,
+            "cds": cds,
+        }
         return df_dict
--- main.py
+++ main.py
@@ -4,12 +4,27 @@
 from shiny import App, ui, render, reactive
 from data import Data
 import logging
+import pandas as pd
 
 # import shinyswatch
 from shinywidgets import render_plotly, output_widget
 import plotly.express as px
 from cache_pandas import timed_lru_cache
 import plotly.graph_objects as go
+
+@timed_lru_cache(seconds=None, maxsize=None)
+def dataF():
+    logging.info("Fetching data in dataF()")
+    df = Data()
+    vix_data = df.vix_history()
+    policy_rate1, policy_rate2, policy_rate3 = df.policy_rate()
+    fx = df.forex_exchange()
+    cds = df.cds()
+    liquidity = df.liquidity()
+    logging.info("Completed fetching data in dataF()")
+    return vix_data, policy_rate1, policy_rate2, policy_rate3, fx, cds, liquidity
+
+all_var = dataF()
 
 app_ui = ui.page_fixed(
     ui.navset_tab(
@@ -20,14 +35,20 @@ app_ui = ui.page_fixed(
                     ui.input_selectize(
                         "var",
                         "Policy Rate",
-                        choices=["All region", "Selected region 1", "Selected region 2"]
-                    )
+                        choices=[
+                            "All region",
+                            "Selected region 1",
+                            "Selected region 2",
+                        ],
+                    ),
+                    ui.input_date_range("date_range", "Select Date Range:", 
+                        start=all_var[0]["Date"].min(), end=all_var[0]["Date"].max())
                 ),
-            output_widget('hist'),
-            output_widget('hist2'),
-            output_widget('hist1'),
-            output_widget('hist3'),
-            output_widget('hist4'),
+                output_widget("hist"),
+                output_widget("hist1"),
+                output_widget("hist2"),
+                output_widget("hist3"),
+                output_widget("hist4"),
             ),
         )
     )
@@ -35,56 +56,76 @@ app_ui = ui.page_fixed(
 
 
 def server(input, output, session):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
 
-    @timed_lru_cache(seconds=None, maxsize=None)
-    def dataF():
-        logging.info("Fetching data in dataF()")
-        df = Data()
-        vix_data = df.vix_history()
-        policy_rate1, policy_rate2, policy_rate3 = df.policy_rate()
-        fx = df.forex_exchange()
-        logging.info("Completed fetching data in dataF()")
-        return vix_data, policy_rate1, policy_rate2, policy_rate3, fx
-    @timed_lru_cache(seconds=None, maxsize=None)
-    def dataF2():
-        df = Data()
-        cds = df.cds()
-        liquidity = df.liquidity()
-        return cds, liquidity
+    @reactive.calc
+    def filtered_data():
+        start_date, end_date = input.date_range()
+        start_date = pd.Timestamp(start_date)
+        end_date = pd.Timestamp(end_date)
+        return all_var[0][(all_var[0]["Date"] >= start_date) & (all_var[0]["Date"] <= end_date)]
+
     @render_plotly
-    def hist():        
-        vix_data, policy_rate1, policy_rate2, policy_rate3, fx = dataF()
-        return px.line(vix_data, x='Date', y='CLOSE', title='VIX Liquidity')
+    def hist():
+        vix = filtered_data()#all_var[0]
+        # vix_df = filtered_data(vix)
+        return px.line(vix, x="Date", y="CLOSE", title="VIX Liquidity")
+
     @render_plotly
-    def hist1():        
-        vix_data, policy_rate1, policy_rate2, policy_rate3, fx = dataF()
+    def hist1():
+        policy_rate1, policy_rate2, policy_rate3 = all_var[1],all_var[2],all_var[3]
         policy_dict = {
             "All region": policy_rate1,
             "Selected region 1": policy_rate2,
-            "Selected region 2": policy_rate3
+            "Selected region 2": policy_rate3,
         }
         policy_rate = policy_dict.get(input.var(), policy_rate1)
-        return px.line(policy_rate, x="Date", y="Value", color="Region", markers=True, title="Policy Rate")
+        return px.line(
+            policy_rate,
+            x="Date",
+            y="Value",
+            color="Region",
+            markers=True,
+            title="Policy Rate",
+        )
+
     @render_plotly
     def hist2():
-        vix_data, policy_rate1, policy_rate2, policy_rate3, fx = dataF()
-        fig = px.histogram(fx, x="Value", y="Region",
-            color='index', barmode='group',
+        fx = all_var[4]
+        fig = px.histogram(
+            fx,
+            x="Value",
+            y="Region",
+            color="index",
+            barmode="group",
             # orientation="h",
-            height=400, title='Forex Exchange')
+            height=400,
+            title="Forex Exchange",
+        )
         return fig
+
     @render_plotly
     def hist3():
-        cds, liquidity = dataF2()
-        return px.line(cds, x="Date", y="Value", color="Region", markers=True, title="CDS")
+        cds = all_var[5]
+        return px.line(
+            cds, x="Date", y="Value", color="Region", markers=True, title="CDS"
+        )
+
     @render_plotly
     def hist4():
-        cds, liquidity = dataF2()
-        fig = px.histogram(liquidity, x="TIME_PERIOD", y="OBS_VALUE",
+        liquidity = all_var[6]
+        fig = px.histogram(
+            liquidity,
+            x="TIME_PERIOD",
+            y="OBS_VALUE",
             # color='index', barmode='group',
             # orientation="h",
-            height=400, title='Liquidity')
+            height=400,
+            title="Liquidity",
+        )
         return fig
+
 
 app = App(app_ui, server)
